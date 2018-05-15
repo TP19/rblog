e4d607e1fa5a0296628873cd0ad22b9a9215290b
Teodor Ponomariov <teodorp@protonmail.com>
2018-05-15 22:18:24 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rblog::Application.config.secret_token = 'f1511652fded98fd63a73adaeff152f53ff23020054cdbfa7bb94bbe05c05ab8003cea29792ff09fcc634831ef0ac65b0efa7a91783b7f38be2e233cdef51a7b'
