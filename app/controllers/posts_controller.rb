71b76bb513f64c3109b29916a7804d5f155fcbc6
Teodor Ponomariov <teodorp@protonmail.com>
2018-05-16 21:21:58 +0200

    update

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+	def index
+		@content_first = 'First Content here';
+
+	end 
+
+	def new
+
+	end
+
+	def create 
+
+	end
+
+	def edit
+		
+	end
+
+	def update
+
+	end
+
+	def show
+
+	end
+
+	def destroy
+
+	end
+end
