1886e9ded40f5e393e5d721329d0744048d02f19
Teodor Ponomariov <teodorp@protonmail.com>
2018-05-27 14:13:52 +0200

    commit to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -57,9 +57,9 @@ ActiveRecord::Schema.define(:version => 20180527004608) do
     t.string   "title"
     t.text     "body"
     t.integer  "category_id"
-    t.integer  "author_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.integer  "admin_user_id"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
 end
